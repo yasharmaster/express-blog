5f44e4a9745842d46b46961a1c7a421057196bf8
Yash Sharma <ihm2014006@iiita.ac.in>
2016-03-31 13:31:18 +0000

    Add edit user function and partial

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-   
-   def new
+    
+    def new
        @user = User.new
-   end
+    end
 
     def create
         @user = User.new(user_params)
@@ -11,6 +11,20 @@ class UsersController < ApplicationController
             redirect_to articles_path
         else
             render 'new'
+        end
+    end
+    
+    def edit
+       @user = User.find(params[:id]) 
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your details were successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
         end
     end
     
